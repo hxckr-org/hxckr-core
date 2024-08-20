5332cab4d025535bc2eac3ed08f81489f1a844c7
nully0x <olusegunsamson@live.com>
2024-08-20 22:46:25 +0100

    change postgres to alpineV15 and change cred

--- flake.nix
+++ flake.nix
@@ -18,19 +18,22 @@
         ];
 
         shellHook = ''
+          # Change the prompt color to blue when in the Nix shell
+          export PS1="\[\033[01;34m\]\u@\h:\w\[\033[00m\]\$ "
+          echo "You are now in the Nix shell with a blue prompt!"
           # PostgreSQL setup
           export POSTGRES_DATA="$PWD/postgres_data"
           export POSTGRES_HOST="localhost"
           export POSTGRES_PORT=5432
-          export POSTGRES_USER="my_user"
-          export POSTGRES_PASSWORD="my_password"
-          export POSTGRES_DB="my_database"
+          export POSTGRES_USER="postgres"
+          export POSTGRES_PASSWORD="postgres"
+          export POSTGRES_DB="postgres"
           mkdir -p "$POSTGRES_DATA"
           docker-compose --file ${pkgs.writeText "docker-compose.yml" ''
             version: '3'
             services:
               postgres:
-                image: postgres:14
+                image: postgres:15-alpine
                 restart: always
                 environment:
                   POSTGRES_USER: $POSTGRES_USER
@@ -54,7 +57,7 @@
             version: '3'
             services:
               postgres:
-                image: postgres:14
+                image: postgres:15-alpine
                 restart: always
                 environment:
                   POSTGRES_USER: $POSTGRES_USER

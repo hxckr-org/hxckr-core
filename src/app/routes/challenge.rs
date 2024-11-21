23504f78683488157d395d091385ca545c9c5288
Theophilus <isahtheophilus@gmail.com>
2024-11-21 22:52:43 +0100

    feat: add endpoint to get all user challenge attempts

--- src/app/routes/challenge.rs
+++ src/app/routes/challenge.rs
@@ -2,11 +2,11 @@ use crate::{
     app::auth::middleware::SessionInfo,
     service::database::{
         conn::DbPool,
-        models::{Challenge, User},
+        models::{Challenge, Repository, User},
     },
     shared::{
-        errors::{CreateChallengeError, GetChallengeError, RepositoryError},
-        primitives::{ChallengeMode, Difficulty, UserRole},
+        errors::{CreateChallengeError, GetChallengeError, GetRepositoryError, RepositoryError},
+        primitives::{ChallengeMode, Difficulty, Period, UserRole},
     },
 };
 use actix_web::{
@@ -20,6 +20,7 @@ pub fn init() -> Scope {
     web::scope("/challenge")
         .route("", web::get().to(get_challenge))
         .route("", web::post().to(create_challenge))
+        .route("/attempts", web::get().to(get_all_attempts))
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -38,6 +39,12 @@ struct GetChallengeQuery {
     repo_url: Option<String>,
     difficulty: Option<Difficulty>,
     mode: Option<ChallengeMode>,
+}
+
+#[derive(serde::Deserialize)]
+struct GetAllAttemptsQuery {
+    period: Option<Period>,
+    challenge_id: Option<Uuid>,
 }
 
 async fn create_challenge(
@@ -226,4 +233,46 @@ async fn get_challenge(
         _ => RepositoryError::DatabaseError(e.to_string()).into(),
     })?;
     Ok(challenge)
+}
+
+async fn get_all_attempts(
+    query: web::Query<GetAllAttemptsQuery>,
+    pool: web::Data<DbPool>,
+) -> Result<HttpResponse, Error> {
+    let period = match query.period.as_ref() {
+        Some(period) => period,
+        None => &Period::AllTime,
+    };
+    let challenge_id = match query.challenge_id.as_ref() {
+        Some(challenge_id) => challenge_id,
+        None => {
+            return Err(Error::from(RepositoryError::BadRequest(
+                "Challenge id is required".to_string(),
+            )))
+        }
+    };
+
+    let mut conn = pool.get().map_err(|e| {
+        error!("Error getting db connection from pool: {}", e);
+        RepositoryError::DatabaseError(e.to_string())
+    })?;
+
+    let attempts = Repository::get_all_repos(
+        &mut conn,
+        period,
+        challenge_id,
+    )
+    .map(|attempts| HttpResponse::Ok().json(attempts))
+    .map_err(|e| match e.downcast_ref() {
+        Some(RepositoryError::FailedToGetRepository(GetRepositoryError(e))) => {
+            RepositoryError::FailedToGetRepository(GetRepositoryError(
+                diesel::result::Error::DatabaseError(
+                    diesel::result::DatabaseErrorKind::Unknown,
+                    Box::new(e.to_string()),
+                ),
+            ))
+        }
+        _ => RepositoryError::DatabaseError(e.to_string()).into(),
+    })?;
+    Ok(attempts)
 }
